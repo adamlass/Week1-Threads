6b92a055df4c96c4fc18a86622c06f6d4e13d83c
adamlass <33089759+adamlass@users.noreply.github.com>
2018-08-20 20:17:57 +0200

    Exercise 2 done

--- src/Day1/Exercise2.java
+++ src/Day1/Exercise2.java
@@ -4,6 +4,7 @@
 //Have the threads print an identifier with each number, so you can see which thread prints what (use the iteration variable from the outer loop where the thread is created).
 //a) describe the output. Is it what you expected?
 //b) If the threads do not interleave, can you make them, by making the threads sleep for a short period of time? (think milliseconds).
+
 package Day1;
 
 /**
@@ -12,6 +13,16 @@ package Day1;
  */
 public class Exercise2 {
     public static void main(String[] args) {
-        
+        new Exercise2().threadmaker(600);
+    }
+    
+    public void threadmaker(int numThreads){
+        for (int i = 0; i < numThreads; i++) {
+            new Thread(() -> {
+                for (int j = 1; j <= 100; j++) {
+                    System.out.println(Thread.currentThread().getName() + " - Number: " + j);
+                }
+            }).start();
+        }
     }
 }
